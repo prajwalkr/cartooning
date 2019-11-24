05f573e5919528c219e2ab2fa17ace762cb30606
Prajwal <prajwalrenukanand@gmail.com>
2019-11-24 19:34:40 +0530

    Corrections in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 pip -r requirements.txt
-mkdir results
-mkdir data
+mkdir -p results
+mkdir -p data
 cd data
 wget https://github.com/codeniko/shape_predictor_81_face_landmarks/blob/master/shape_predictor_81_face_landmarks.dat
 cd ..
