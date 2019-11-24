1d5315fc01dbaa47ad5dbf61a8544794fc4c3aac
Sindhu <sindhuhegde56@gmail.com>
2019-11-24 19:50:07 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@ pip -r requirements.txt
 mkdir -p results
 mkdir -p data
 cd data
-wget https://github.com/codeniko/shape_predictor_81_face_landmarks/blob/master/shape_predictor_81_face_landmarks.dat
+wget https://github.com/codeniko/shape_predictor_81_face_landmarks/raw/master/shape_predictor_81_face_landmarks.dat
 cd ..
